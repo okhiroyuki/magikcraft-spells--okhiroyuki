09643e34ba488da10a5735c9edec426d10f2ea95
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2018-02-18 06:37:02 +0000

    Update fly.ts

--- fly.ts
+++ fly.ts
@@ -2,7 +2,7 @@ const magik = magikcraft.io;
 
 function fly(){
     const location = magik.hic();
-    const nearbyEntities = location.getWorld().getNearbyEntities(location, 8, 8, 8);
+    const nearbyEntities = location.getWorld().getNearbyEntities(location, 0, 0, 0);
     nearbyEntities.forEach(function(entity){
         magik.dixit(entity.getName());
     });
